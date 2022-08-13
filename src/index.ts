fae871d47274a7f1fa6e0eac6a818f75ecc96d28
NSUWAL123 <suwalnishit@gmail.com>
2022-08-13 20:08:20 +0545

    Made migration files for database

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Request, Response, Application } from 'express';
-
 import appRouter from './routes';
 import { notFound } from './middlewares/notFound';
 import { errorHandler } from './middlewares/errorHandler';
+import logger from './misc/logger';
 
 dotenv.config();
-
 const app: Application = express();
 
 app.use(express.json());
-
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
@@ -19,10 +17,8 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use(appRouter);
-
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
